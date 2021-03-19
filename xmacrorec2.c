89e38bb3af893ec697c14a69f32831494d3a5117
linuxCowboy <lxc797@gmail.com>
2021-03-19 01:34:18 +0100

    final MotionNotify

--- xmacrorec2.c
+++ xmacrorec2.c
@@ -405,6 +405,8 @@ void eventCallback (XPointer priv, XRecordInterceptData * d)
 			/* should we stop looping, i.e. did the user press the quitkey? */
 			if (HasQuitKey && detail == QuitKey)
 			{
+				fprintf (fdout, MOTION_NOTIFY" %i %i\n", rootx, rooty);
+
 				/* yep, no more loops */
 				fprintf (stderr, "Got QuitKey, so exiting...\n");
 				p->doit = 0;
